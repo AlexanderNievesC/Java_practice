ee9cb3a225ce3949d3b2e8208ab04fa3512253bb
AlexanderNievesC <paulalexander.nieves@gmail.com>
2024-10-17 20:01:58 -0500

    chore: Week test

--- Dias.java
+++ Dias.java
@@ -6,10 +6,32 @@ public class Dias {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Ingrese un numero del 1 al 7: ");
-        int numero = nextInt();
+        int dia = scanner.nextInt();
+
+        switch (dia) {
+            case 1:
+                System.out.println("Lunes");
+                break;
+            case 2:
+                System.out.println("Martes");
+                break;
+            case 3:
+                System.out.println("Miercoles");
+                break;
+            case 4:
+                System.out.println("Jueves");
+                break;
+            case 5:
+                System.out.println("Viernes");
+                break;
+            case 6:
+                System.out.println("Sabado");
+                break;
+            default:
+                System.out.println("Domingo");
+        }
 
         scanner.close();
-
     }
 
 }
